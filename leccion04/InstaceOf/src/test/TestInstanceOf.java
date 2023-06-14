2a75965198197dc2c47aa7e16d2621b76a47e0a7
Mauricio <mauriciosandrea2019@gmail.com>
2023-06-13 21:43:48 -0300

    leccion05

--- leccion04/InstaceOf/src/test/TestInstanceOf.java
+++ leccion04/InstaceOf/src/test/TestInstanceOf.java
@@ -17,17 +17,18 @@ public class TestInstanceOf {
     public static void determinarTipo(Empleado empleado){
         if(empleado instanceof Gerente){
             System.out.println("Es de tipo Gerente");
-            Gerente gerente = (Gerente)empleado;
-            System.out.println("gerente = " + gerente.getDepartamento());
+            Gerente gerente = (Gerente)empleado;//hacemos la conversion de esta menera manual.
+            //System.out.println("gerente = " + gerente.getDepartamento());
+            ((Gerente) empleado).getDepartamento();//lo asigna el ide automaticamente hace la conversion
             
         }
         else if(empleado instanceof Empleado){
             System.out.println("Es de tipo Emplado");
-            Gerente gerente = (Gerente)empleado;
-            System.out.println("gerente = " + gerente.getDepartamento());
+            //Gerente gerente = (Gerente)empleado;
+        //    System.out.println("gerente = " + gerente.getDepartamento());
         }
         else if(empleado instanceof Object){
-            System.out.println("Es de tipo object");
+           // System.out.println("Es de tipo object");
         }
     }
      
